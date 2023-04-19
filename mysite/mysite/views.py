4be351d3c24dbd8b9c53243ca0b53092550a3223
Mrinmay Das <mrinmaydas2604@gmail.com>
2023-04-19 22:04:01 +0530

    pipelines done

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,17 +1,17 @@
 #     I have created this file Mrinmay
 from django.http import HttpResponse
 def index(request):
-    return HttpResponse("Home-page")
+    return HttpResponse('''Home-page <a href= "removepunc">remo</a>''')
 
 def removepunc(request):
-    return HttpResponse("removepunc  <a href="">")
+    return HttpResponse('''removepunc  <a href= "/">back</a>''')
 
 def capfirst(request):
-    return HttpResponse("captalize it")
+    return HttpResponse('''captalize it <a href= "/">back</a>''')
 
 def spacerem(request):
-    return HttpResponse("space removal")
+    return HttpResponse('''space removal <a href= "/">back</a>''')
 
 def counter(request):
-    return HttpResponse("word counter")
+    return HttpResponse('''word counter <a href= "/">back</a>''')
 
