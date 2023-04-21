6922ea7144e5948c4610ee3d83bb868b52ab3203
Mrinmay Das <mrinmaydas2604@gmail.com>
2023-04-22 00:23:46 +0530

    yep done

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--4ix0fe(x9l*54--*zf9rmme6dk#e+dj9t7g*boaf8k%6^mq^*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
